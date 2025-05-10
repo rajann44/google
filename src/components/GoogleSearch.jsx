0007ec9b2880d8757e7f4c2025845f32ef860f1a
rajann44 <rajann44@gmail.com>
2025-05-10 17:11:35 +0200

    first commit

---
+++ src/components/GoogleSearch.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import './GoogleSearch.css';
+
+const FEELING_PHRASES = [
+  "I'm Feeling Happy",
+  "I'm Feeling Creative",
+  "I'm Feeling Adventurous",
+  "I'm Feeling Inspired",
+  "I'm Feeling Hungry",
+  "I'm Feeling Lucky"
+];
+
+const GoogleSearch = () => {
+  const [feelingText, setFeelingText] = useState("I'm Feeling Lucky");
+
+  const handleFeelingHover = () => {
+    let newPhrase;
+    do {
+      newPhrase = FEELING_PHRASES[Math.floor(Math.random() * FEELING_PHRASES.length)];
+    } while (newPhrase === feelingText);
+    setFeelingText(newPhrase);
+  };
+
+  const handleFeelingLeave = () => {
+    setFeelingText("I'm Feeling Lucky");
+  };
+
+  return (
+    <div className="google-search-container">
+      <div className="search-wrapper">
+        <div className="logo">
+          <svg className="lnXdpd" height="92" viewBox="0 0 272 92" width="272" xmlns="http://www.w3.org/2000/svg"><path d="M115.75 47.18c0 12.77-9.99 22.18-22.25 22.18s-22.25-9.41-22.25-22.18C71.25 34.32 81.24 25 93.5 25s22.25 9.32 22.25 22.18zm-9.74 0c0-7.98-5.79-13.44-12.51-13.44S80.99 39.2 80.99 47.18c0 7.9 5.79 13.44 12.51 13.44s12.51-5.55 12.51-13.44z" fill="#EA4335"></path><path d="M163.75 47.18c0 12.77-9.99 22.18-22.25 22.18s-22.25-9.41-22.25-22.18c0-12.85 9.99-22.18 22.25-22.18s22.25 9.32 22.25 22.18zm-9.74 0c0-7.98-5.79-13.44-12.51-13.44s-12.51 5.46-12.51 13.44c0 7.9 5.79 13.44 12.51 13.44s12.51-5.55 12.51-13.44z" fill="#FBBC05"></path><path d="M209.75 26.34v39.82c0 16.38-9.66 23.07-21.08 23.07-10.75 0-17.22-7.19-19.66-13.07l8.48-3.53c1.51 3.61 5.21 7.87 11.17 7.87 7.31 0 11.84-4.51 11.84-13v-3.19h-.34c-2.18 2.69-6.38 5.04-11.68 5.04-11.09 0-21.25-9.66-21.25-22.09 0-12.52 10.16-22.26 21.25-22.26 5.29 0 9.49 2.35 11.68 4.96h.34v-3.61h9.25zm-8.56 20.92c0-7.81-5.21-13.52-11.84-13.52-6.72 0-12.35 5.71-12.35 13.52 0 7.73 5.63 13.36 12.35 13.36 6.63 0 11.84-5.63 11.84-13.36z" fill="#4285F4"></path><path d="M225 3v65h-9.5V3h9.5z" fill="#34A853"></path><path d="M262.02 54.48l7.56 5.04c-2.44 3.61-8.32 9.83-18.48 9.83-12.6 0-22.01-9.74-22.01-22.18 0-13.19 9.49-22.18 20.92-22.18 11.51 0 17.14 9.16 18.98 14.11l1.01 2.52-29.65 12.28c2.27 4.45 5.8 6.72 10.75 6.72 4.96 0 8.4-2.44 10.92-6.14zm-23.27-7.98l19.82-8.23c-1.09-2.77-4.37-4.7-8.23-4.7-4.95 0-11.84 4.37-11.59 12.93z" fill="#EA4335"></path><path d="M35.29 41.41V32H67c.31 1.64.47 3.58.47 5.68 0 7.06-1.93 15.79-8.15 22.01-6.05 6.3-13.78 9.66-24.02 9.66C16.32 69.35.36 53.89.36 34.91.36 15.93 16.32.47 35.3.47c10.5 0 17.98 4.12 23.6 9.49l-6.64 6.64c-4.03-3.78-9.49-6.72-16.97-6.72-13.86 0-24.7 11.17-24.7 25.03 0 13.86 10.84 25.03 24.7 25.03 8.99 0 14.11-3.61 17.39-6.89 2.66-2.66 4.41-6.46 5.1-11.65l-22.49.01z" fill="#4285F4"></path></svg>
+        </div>
+        
+        <div className="search-box">
+          <div className="search-input-wrapper">
+            <svg className="search-icon" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+            </svg>
+            <input 
+              type="text" 
+              className="search-input"
+              placeholder="Search Google or type a URL"
+            />
+            <div className="search-icons">
+              <svg className="mic-icon" focusable="false" viewBox="0 -960 960 960" xmlns="http://www.w3.org/2000/svg"><path fill="var(--bbQxAb)" d="M480-400q-50 0-85-35t-35-85v-240q0-50 35-85t85-35q50 0 85 35t35 85v240q0 50-35 85t-85 35Zm-40 280v-123q-104-14-172-93t-68-184h80q0 83 58.5 141.5T480-320q83 0 141.5-58.5T680-520h80q0 105-68 184t-172 93v123h-80Z"></path></svg>
+              <svg className="camera-icon" focusable="false" viewBox="0 -960 960 960" xmlns="http://www.w3.org/2000/svg"><path fill="var(--bbQxAb)" d="M480-320q-50 0-85-35t-35-85q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35Zm240 160q-33 0-56.5-23.5T640-240q0-33 23.5-56.5T720-320q33 0 56.5 23.5T800-240q0 33-23.5 56.5T720-160Zm-440 40q-66 0-113-47t-47-113v-80h80v80q0 33 23.5 56.5T280-200h200v80H280Zm480-320v-160q0-33-23.5-56.5T680-680H280q-33 0-56.5 23.5T200-600v120h-80v-120q0-66 47-113t113-47h80l40-80h160l40 80h80q66 0 113 47t47 113v160h-80Z"></path></svg>
+            </div>
+          </div>
+        </div>
+
+        <div className="search-buttons">
+          <input
+            className="gNO89b"
+            type="submit"
+            value="Google Search"
+            aria-label="Google Search"
+            name="btnK"
+            tabIndex="0"
+          />
+          <input
+            className="gNO89b"
+            type="submit"
+            value={feelingText}
+            aria-label="I'm Feeling Lucky"
+            name="btnI"
+            tabIndex="0"
+            onMouseEnter={handleFeelingHover}
+            onMouseLeave={handleFeelingLeave}
+          />
+        </div>
+
+        <div className="language-options">
+          Google offered in: <a href="#">Deutsch</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GoogleSearch; 
